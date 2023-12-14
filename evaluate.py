66ddc3653b8c9d2ff59338a1011ac3f6633f36aa
ezhang <ezhang@kyros.ai>
2023-12-14 17:03:59 -0500

    refactored train, eval, utils

--- evaluate.py
+++ evaluate.py
@@ -1,14 +1,43 @@
-# Load model
-model = MyUNet("VGGNet19", 8).to(device)
-checkpoint = torch.load('./MyUNet_vggnet19_final_checkpoint.pth', map_location=torch.device('cpu'))
-model.load_state_dict(checkpoint['state_dict'])
+from argparse import ArgumentParser
+from models.unet import UNet
+import torch
+from utils.utils import criterion
 
-evaluate_model(2)
-
-import matplotlib.pyplot as plt
-history['train_loss'].iloc[100:].plot();
-plt.xlabel("Epoch")
-plt.ylabel("Training Loss") #remember to save the figure
-
-series = history.dropna()['dev_loss']
-plt.scatter(series.index, series);
+def evaluate_model(dev_loader, n_epochs):
+    model.eval()
+    
+    for epoch in range(n_epochs):
+        loss = 0
+        
+        with torch.no_grad():
+            for img_batch, mask_batch, regr_batch in dev_loader:
+                img_batch = img_batch.to(device)
+                mask_batch = mask_batch.to(device)
+                regr_batch = regr_batch.to(device)
+                
+                output = model(img_batch)
+                
+                loss += criterion(output, mask_batch, regr_batch, size_average=False).data
+                
+        loss /= len(dev_loader.dataset)
+        print('Dev loss: {:.4f}'.format(loss))
+    
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--model', type=str, default="VGGNet-11")
+    parser.add_argument('-n', '--n_epochs', type=int, default=5)
+    args = parser.parse_args()
+    
+    # choose device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    n_epochs = args.n_epochs
+    backbone_model = args.models
+    n_classes = 8
+    
+    # Load model
+    model = UNet(backbone_model, n_classes).to(device)
+    checkpoint = torch.load(f'./UNet_{backbone_model}.pth', map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['state_dict'])
+    
+    evaluate_model(devloader, 2)
--- models/unet.py
+++ models/unet.py
@@ -1,52 +1,119 @@
-class MyUNet(nn.Module):
-    '''Mixture of previous classes'''
-    def __init__(self, model, n_classes):
-        super(MyUNet, self).__init__()
-        # self.base_model = EfficientNet.from_pretrained('efficientnet-b0')
-        # self.base_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
-        # self.base_model = ResNet18(3, n_classes)
-        if model == "VGGNet11":
-            self.base_model = VGGNet11(3, n_classes)
-        elif model == "VGGNet19":
-            self.base_model = VGGNet19(3, n_classes)
-        elif model == "ResNet18":
-            self.base_model = ResNet18(3, n_classes)
+import torch
+import torch.nn as nn
+from resnet import ResNet
+from vggnet import VGGNet
+from utils.utils import get_mesh
 
-        self.conv0 = double_conv(5, 64) # changed 5 to 3
-        self.conv1 = double_conv(64, 128)
-        self.conv2 = double_conv(128, 512)
-        self.conv3 = double_conv(512, 1024)
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(up, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv = UNetBlock(in_ch, out_ch)
+
+    def forward(self, x1, x2=None):
+        x1 = self.up(x1)
+
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = nn.functional.pad(x1, (diffX // 2, diffX - diffX//2,
+                        diffY // 2, diffY - diffY//2))
+
+        if x2 is not None:
+            x = torch.cat([x2, x1], dim=1)
+        else:
+            x = x1
+        x = self.conv(x)
+        return x
+
+class UNetBlock(nn.Module):
+    '''
+    UNet block
+
+    Arg(s):
+        n_input_feature : int
+            number of input feature channels
+        n_output_feature : int
+            number of output feature channels i.e. number of filters to use
+    '''
+
+    def __init__(self, n_input_feature, n_output_feature):
+        super(UNetBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            n_input_feature,
+            n_output_feature,
+            kernel_size=3,
+            padding=1,
+        )
+
+        self.conv2 = nn.Conv2d(
+            n_output_feature,
+            n_output_feature,
+            kernel_size=3,
+            padding=1,
+        )
+
+        self.bn = nn.BatchNorm2d(n_output_feature)
+        
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        '''
+        Forward input x through a basic UNet block
+
+        Arg(s):
+            x : torch.Tensor[float32]
+                N x C x H x W input tensor
+        Returns:
+            torch.Tensor[float32] : N x K x h x w output tensor
+        '''
+
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        return x
+    
+    
+class UNet(nn.Module):
+    def __init__(self, model, n_classes):
+        super(UNet, self).__init__()
+        model_type = model.split("-")
+        if model_type[0] == "VGGNet":
+            self.base_model = VGGNet(3, int(model_type[1]))
+        elif model_type[0] == "ResNet":
+            self.base_model = ResNet(3, int(model_type[1]))
+
+        self.conv0 = UNetBlock(5, 64) 
+        self.conv1 = UNetBlock(64, 128)
+        self.conv2 = UNetBlock(128, 512)
+        self.conv3 = UNetBlock(512, 1024)
 
         self.mp = nn.MaxPool2d(2)
 
         self.up1 = up(128 + 1024, 512)
-        # self.up1 = up(2304, 512)
         self.up2 = up(512 + 512, 256)
+        
         self.outc = nn.Conv2d(256, n_classes, 1)
-
 
     def forward(self, x):
         batch_size = x.shape[0]
         mesh1 = get_mesh(batch_size, x.shape[2], x.shape[3])
         x0 = torch.cat([x, mesh1], 1)
-        x1 = self.mp(self.conv0(x0)) # changed x0 to x
+        x1 = self.mp(self.conv0(x0)) 
         x2 = self.mp(self.conv1(x1))
         x3 = self.mp(self.conv2(x2))
         x4 = self.mp(self.conv3(x3))
 
-        # N, C, W, H
-        # feature extractor downsample
-        # x_center = x[:, :, :, IMG_WIDTH // 8: -IMG_WIDTH // 8] # centered x
-        # feats = self.base_model.extract_features(x_center) # (4, 1280, 10, 24) shape
         feats_var = self.base_model(x)
         feats = feats_var.data
-        # print("shape of feats: ", feats.shape)
-        # bg = torch.zeros([feats.shape[0], feats.shape[1], feats.shape[2], feats.shape[3] // 8]).to(device)
-        # feats = torch.cat([bg, feats, bg], 3)
-
-        # Add positional info
-        # mesh2 = get_mesh(batch_size, feats.shape[2], feats.shape[3])
-        # feats = torch.cat([feats, mesh2], 1)
 
         x = self.up1(feats, x4)
         x = self.up2(x, x3)
