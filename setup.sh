468cc1cccdec1af52ffad51fbe8dbab5d8f672ed
ezhang <ezhang@kyros.ai>
2023-12-15 23:11:54 -0500

    finished shell script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo "downloading data"
 curl -O -L 'https://www.dropbox.com/scl/fi/h3au34o4p4h2qbf5b6kq8/data.zip?rlkey=4qyh1d00epi57ub5yrj5drv64&dl=0'
-unzip -o data.zip
+unzip -oq data.zip
 echo "finished downloading data"
 echo "preprocessing data"
 python preprocessing.py
