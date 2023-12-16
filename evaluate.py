05c510ef78c4c3e4d790c55881f29ab9fd458277
杨柳 <yangliu@yanglius-Air-2.stdusr.yale.internal>
2023-12-15 20:08:18 -0500

    add preprocessing and map calculation

--- evaluate.py
+++ evaluate.py
@@ -2,36 +2,82 @@ from argparse import ArgumentParser
 from models.unet import UNet
 import torch
 from utils.utils import criterion
+import pandas as pd
+from CarDataset import CarDataset
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from preprocessing import convert_3d_to_2d, optimize_xy, clear_duplicates, extract_coords, coords2str
+from mAP import calculate_map_image, plot_map_statistis
 
-def evaluate_model(dev_loader, n_epochs):
+def evaluate_model_loss(model, test_loader, device='cpu'):
     model.eval()
     
-    for epoch in range(n_epochs):
-        loss = 0
-        
-        with torch.no_grad():
-            for img_batch, mask_batch, regr_batch in dev_loader:
-                img_batch = img_batch.to(device)
-                mask_batch = mask_batch.to(device)
-                regr_batch = regr_batch.to(device)
-                
-                output = model(img_batch)
-                
-                loss += criterion(output, mask_batch, regr_batch, size_average=False).data
-                
-        loss /= len(dev_loader.dataset)
-        print('Dev loss: {:.4f}'.format(loss))
+    loss = 0
     
+    with torch.no_grad():
+        for img_batch, mask_batch, regr_batch in test_loader:
+            img_batch = img_batch.to(device)
+            mask_batch = mask_batch.to(device)
+            regr_batch = regr_batch.to(device)
+            
+            output = model(img_batch)
+            
+            loss += criterion(output, mask_batch, regr_batch, size_average=False).data
+            
+    loss /= len(test_loader.dataset)
+    print('Test loss: {:.4f}'.format(loss))
+
+def generate_df_map(filename):
+    names_true = ['id', 'yaw_true', 'pitch_true', 'roll_true', 'x_true', 'y_true', 'z_true']
+    names_pred = ['yaw', 'pitch', 'roll', 'x', 'y', 'z', 'confidence']
+    rot_thre_list = [50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
+    tran_thre_list = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    pred_df = pd.read_csv(filename)
+    pred_df['NewPredictionString'] = pred_df['NewPredictionString'].fillna(" ")
+    pred_df['map'] = [calculate_map_image(img_idx, pred_df, names_true, names_pred, rot_thre_list, tran_thre_list) for img_idx in range(len(pred_df))]
+    plot_map_statistis(pred_df)
+    return pred_df
+
+def evaluate_model_prediction(model, test_loader, device, df_test, filename):
+    predictions = []
+
+    model.eval()
+
+    for img, _, _ in tqdm(test_loader):
+        with torch.no_grad():
+            output = model(img.to(device))
+        output = output.data.cpu().numpy()
+        for out in output:
+            coords = extract_coords(out)
+            s = coords2str(coords)
+            predictions.append(s)
+
+    df_test["NewPredictionString"] = predictions
+    df_test.to_csv(filename,index=False)
+    generate_df_map(filename)
+
+    return predictions
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-m', '--model', type=str, default="VGGNet-11")
-    parser.add_argument('-n', '--n_epochs', type=int, default=5)
+    parser.add_argument('-b', '--batch_size', type=int, default=4)
+    parser.add_argument('-d', '--device', type=str, default='cpu')
+    parser.add_argument('-ls', '--calculate_map', type=bool, default=True)
+    parser.add_argument('-f', '--filename', type=str, default="predictions.csv")
     args = parser.parse_args()
     
+    # load data
+    test_images_dir = args.path + 'train_images/{}.jpg'
+    df_test = pd.read_csv("test.csv")
+    test_dataset = CarDataset(df_test, test_images_dir, training=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+
     # choose device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = 'cuda' if args.device == 'gpu' or args.device == 'cuda' else 'cpu'
+    device = torch.device(device)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    n_epochs = args.n_epochs
     backbone_model = args.models
     n_classes = 8
     
@@ -40,4 +86,9 @@ if __name__ == '__main__':
     checkpoint = torch.load(f'./UNet_{backbone_model}.pth', map_location=torch.device('cpu'))
     model.load_state_dict(checkpoint['state_dict'])
     
-    evaluate_model(devloader, 2)
+    if (args.calculate_map == True):
+        evaluate_model_prediction(model, test_loader, device, df_test, args.filename)
+    else:
+        evaluate_model_loss(model, test_loader, device)
+
+
--- train.py
+++ train.py
@@ -6,6 +6,9 @@ from models.unet import UNet
 from utils.utils import criterion
 from argparse import ArgumentParser
 from utils.dataset import CarDataset
+from torch.utils.data import DataLoader
+import pickle
+import pandas as pd
 
 def train_model(model, optimizer, train_loader, n_epochs):
     model.train()
@@ -38,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--model', type=str, default="VGGNet-11")
     parser.add_argument('-n', '--n_epochs', type=int, default=5)
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.001)
+    parser.add_argument('-b', '--batch_size', type=int, default=4)
     args = parser.parse_args()
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -49,15 +53,16 @@ if __name__ == "__main__":
     lr = args.learning_rate
     
     train_images_dir = args.path + 'train_images/{}.jpg'
-    df_train, df_dev = train_test_split(train, test_size=0.01, random_state=42)
+    df_train = pd.read_csv("train.csv")
     train_dataset = CarDataset(df_train, train_images_dir, training=True)
- 
+    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
     # Define model, optimizer, and learning rate scheduler
     model = UNet(backbone_model, n_classes).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=max(n_epochs, 10) * len(train_loader) // 3, gamma=0.1)
     
-    model = train_model(n_epochs)
+    model = train_model(model, optimizer, train_loader, n_epochs)
     
     # save model
     torch.save({'state_dict' : model.state_dict(),'optimizer_state_dict' : optimizer.state_dict(),}, f'./UNet_{backbone_model}.pth')
